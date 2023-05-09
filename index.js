60c271f5e271caf2dd575973f6a7514e62812340
Manjunath B <manjunathb.mca22@rvce.edu.in>
2023-05-09 07:59:43 +0530

    Final day

--- index.js
+++ index.js
@@ -1,55 +1,74 @@
-const observer = new IntersectionObserver((entries)=>{
-    entries.forEach(entry => {
-        console.log(entry.target.classList)
-        if(entry.isIntersecting){
-            
-            entry.target.classList.add("show");
-        }else{
-            entry.target.classList.remove("show")
-        }
-    });
-
-})
-
-const hiddenElementsX = document.querySelectorAll(".hiddenX,.hiddenY,.pop")
-console.log(hiddenElementsX)
-hiddenElementsX.forEach((element)=>{
-    observer.observe(element)
-})
-var mode="light"
-
-
-function darkMode(){
-    if(mode=="light"){
-        mode="dark"
-        document.getElementsByTagName("body")[0].style.backgroundColor="#082032"
-        document.getElementById("navbar").style.backgroundColor="#082032"
-        document.getElementById("logo").style.color="white"
-        document.querySelectorAll(".nav-link,.nav-item").forEach((ele)=>{
-                ele.style.color="white"
-                console.log("done")
-            })
-            document.querySelectorAll(".book,.card,.packages").forEach((ele)=>{
-                ele.style.backgroundColor="#082032"
-                ele.style.color="white"
-                ele.style.textShadow="0px 1px 1px black"
-               
-                console.log(ele)
-            })
-        
-   
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry.target.classList);
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
     }
-    else{
-        mode="light"
-        document.getElementById("navbar").style.backgroundColor="#f9f9f9"
-        document.getElementById("logo").style.color="black"
-        document.querySelectorAll(".nav-link,.nav-item").forEach((ele)=>{
-            ele.style.color=" rgb(69, 67, 66)"
-            console.log("done")
-        })
-        
-        
-    }
-    
-}
+  });
+});
 
+const hiddenElementsX = document.querySelectorAll(".hiddenX,.hiddenY,.pop");
+console.log(hiddenElementsX);
+hiddenElementsX.forEach((element) => {
+  observer.observe(element);
+});
+// var mode = "light";
+
+// function darkMode() {
+//   if (mode == "light") {
+//     mode = "dark";
+//     document.getElementsByTagName("body")[0].style.backgroundColor = "#082032";
+//     document.getElementById("navbar").style.backgroundColor = "#082032";
+//     document.getElementById("logo").style.color = "white";
+//     document.querySelectorAll(".nav-link,.nav-item").forEach((ele) => {
+//       ele.style.color = "white";
+//       console.log("done");
+//     });
+//     document.querySelectorAll(".book,.card,.packages").forEach((ele) => {
+//       ele.style.backgroundColor = "#082032";
+//       ele.style.color = "white";
+//       ele.style.textShadow = "0px 1px 1px black";
+
+//       console.log(ele);
+//     });
+//   } else {
+//     mode = "light";
+//     document.getElementById("navbar").style.backgroundColor = "#f9f9f9";
+//     document.getElementById("logo").style.color = "black";
+//     document.querySelectorAll(".nav-link,.nav-item").forEach((ele) => {
+//       ele.style.color = " rgb(69, 67, 66)";
+//       console.log("done");
+//     });
+//   }
+// }
+
+// let toggle = document.querySelector('input[type="checkbox"]');
+// // console.log(toggle);
+
+// toggle.addEventListener("change", (e) => {
+//   if (e.target.checked) {
+//     document.documentElement.setAttribute("data-theme", "dark");
+//   } else {
+//     document.documentElement.setAttribute("data-theme", "light");
+//   }
+// });
+
+let toggle = document.querySelector('input[type="checkbox"]');
+// console.log(toggle);
+
+toggle.addEventListener("change", (e) => {
+  if (e.target.checked) {
+    console.log("reue");
+    document.documentElement.setAttribute("data-theme", "dark");
+    document.getElementsByTagName("body")[0].style.backgroundColor = "black";
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+    // console.log("cadc");
+  }
+});
+
+
+  
---
+++ val.js
@@ -0,0 +1,47 @@
+var form = document.getElementById('form')
+var name = document.getElementById('name').value
+var email = document.getElementById('email')
+var text =  document.getElementById('err')
+var d = document.getElementById('date')
+var d1 = document.getElementById('date1')
+
+
+function validate(){
+    
+  var patt1 = "[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+  
+  
+  var e = email.value
+ if(e.match(patt1)){
+    // form.classList.add("valid")
+    email.style.borderBottom = '10px solid green'
+    // console.log("coorect");
+ }else{
+    email.style.borderBottom = '10px solid red'
+    email.classList.add("invalid")
+    // console.log('wrong');
+ }
+if(e == ""){
+    email.style.borderBottom = 'none'
+}
+}
+
+var date = new Date()
+var day = date.getDate()
+var m = date.getMonth()
+var y = date.getFullYear()
+// console.log(day,m,y);
+if(m < 10){
+    m = "0" + (m + 1)
+}
+if(day < 10){
+    day = "0" + day
+}
+console.log(`${y}-${m}-${day}`)
+d.setAttribute('min',`${y}-${m}-${day}`)
+// console.log(day,m,y)
+var dd1 = date.getDate()
+if(dd1 < 10){
+    dd1 = "0" + (dd1+1)
+}
+d1.setAttribute('min',`${y}-${m}-${dd1}`)
